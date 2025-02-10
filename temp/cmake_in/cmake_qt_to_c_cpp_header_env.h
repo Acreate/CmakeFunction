4271f973c43a9f5c3874b1b9fc294f73cb4e1626
Acreate <572646005@qq.com>
2025-02-10 18:12:47 +0800

    改进宏模板

--- temp/cmake_in/cmake_qt_to_c_cpp_header_env.h
+++ temp/cmake_in/cmake_qt_to_c_cpp_header_env.h
@@ -5,15 +5,10 @@
 #define CMAKE_Qt6_DIR "${Qt6_DIR}" 
 /// @brief qt cmake 目录
 #define CMAKE_Qt_DIR "${Qt_DIR}" 
-#ifndef CMAKE_QT_VERSION_MAJOR
-    /// @brief qt 主版本号
-    #define CMAKE_QT_VERSION_MAJOR "${QT_VERSION_MAJOR}" 
-#endif // CMAKE_QT_VERSION_MAJOR
-
-#ifndef CMAKE_QT_VERSION
-    /// @brief qt 版本
-    #define CMAKE_QT_VERSION "${QT_VERSION}" 
-#endif // CMAKE_QT_VERSION
+/// @brief qt 主版本号
+#define CMAKE_QT_VERSION_MAJOR "${QT_VERSION_MAJOR}" 
+/// @brief qt 版本
+#define CMAKE_QT_VERSION "${QT_VERSION}" 
 /// @brief qt 工具目录
 #define CMAKE_DEPLOY_QT_HOME "${DEPLOY_QT_HOME}" 
 /// @brief qmake 路径
@@ -30,6 +25,10 @@
 #define CMAKE_Qt${QT_VERSION_MAJOR}ZlibPrivate_DIR "${Qt${QT_VERSION_MAJOR}ZlibPrivate_DIR}"
 /// @brief qt core cmake
 #define CMAKE_Qt${QT_VERSION_MAJOR}Core_DIR "${Qt${QT_VERSION_MAJOR}Core_DIR}"
+/// @brief qt windeployqt 执行文件路径
+#define CMAKE_WINDEPLOYQT_EXECUTABLE "${WINDEPLOYQT_EXECUTABLE}"
+/// @brief qt qmake 执行文件路径
+#define CMAKE_QT_QMAKE_EXECUTABLE "${QT_QMAKE_EXECUTABLE}"
 /// @brief 是否自动调用 ic
 #define CMAKE_CMAKE_AUTOUIC "${CMAKE_AUTOUIC}" 
 /// @brief 是否自动调用 moc
