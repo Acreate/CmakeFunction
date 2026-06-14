1aa41087a86f19c46f141adb6cfbe79d78ddccd4
Acreate <1552397900@qq.com>
2026-06-14 16:57:51 +0800

    增加初始化与全校增量编译选项

--- CmakeFuncs/CmakeFuncsConfig.cmake
+++ CmakeFuncs/CmakeFuncsConfig.cmake
@@ -14,11 +14,32 @@ get_filename_component( abs "${CMAKE_CURRENT_LIST_FILE}" ABSOLUTE )
 string( FIND "${abs}" "${CMAKE_HOME_DIRECTORY}" index )
 
 if( NOT ${index} EQUAL -1 )
-  string( LENGTH "${abs}" _orgStrLen )
-  string( LENGTH "${CMAKE_HOME_DIRECTORY}" _findStrLen )
-  math( EXPR _subLen "${_orgStrLen} - ${_findStrLen}" )
-  string( SUBSTRING "${abs}" ${_findStrLen} ${_subLen} abs )
-  set( abs ".${abs}" )
-endif()
+	string( LENGTH "${abs}" _orgStrLen )
+	string( LENGTH "${CMAKE_HOME_DIRECTORY}" _findStrLen )
+	math( EXPR _subLen "${_orgStrLen} - ${_findStrLen}" )
+	string( SUBSTRING "${abs}" ${_findStrLen} ${_subLen} abs )
+	set( abs ".${abs}" )
+endif( )
+
+function( init_cmake_func )
+	set( CMAKE_C_STANDARD 17 )
+	set( CMAKE_C_STANDARD_REQUIRED ON )
+	set( CMAKE_C_VISIBILITY_PRESET hidden )
+	set( CMAKE_C_EXTENSIONS ON )
+	set( CMAKE_CXX_STANDARD 23 )
+	set( CMAKE_CXX_STANDARD_REQUIRED ON )
+	set( CMAKE_CXX_VISIBILITY_PRESET hidden )
+	set( CMAKE_CXX_EXTENSIONS ON )
+	
+	# qt 资源自动编译
+	set( CMAKE_AUTOMOC ON )
+	set( CMAKE_AUTOUIC ON )
+	set( CMAKE_AUTORCC ON )
+	
+	set( CMAKE_SUPPRESS_REGENERATION ON )
+	## 对于一些特殊环境，应当除去增量编译，否则会导致生成的 .pdb 过大而无法编译。
+	## 其次，对于 qt 来说，会导致调用 main 等主函数后，调试器会尝试发出异常中断—增量编译导致的符号导向异常或释放异常。
+	close_global_compile_batching( )
+endfunction( )
 
 message( "----\n\t\t调用:(${abs}[${CMAKE_CURRENT_FUNCTION}]:${CMAKE_CURRENT_LIST_LINE})行 ->\n\t\t\t消息:列表加载完毕" )
